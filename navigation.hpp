1b6aa379aade83ffb83c46a0106cbf5ec1c5e260
melektron <matteo@elektron.work>
2023-02-08 15:45:28 +0000

    added navigation class

---
+++ navigation.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file navigation.hpp
+ * @author melektron
+ * @brief base class for the navigation system that will be inherited
+ * by any other navigation implementations for the specific robots
+ * @version 0.1
+ * @date 2023-02-08
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+class Navigation
+{
+
+};
