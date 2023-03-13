7e33926ef6e3a61671ba203bc76fdcd02f6da9e3
melektron <matteo@elektron.work>
2023-03-13 14:19:01 +0000

    implemented rotateTo and driveVector

--- navigation.hpp
+++ navigation.hpp
@@ -12,6 +12,8 @@
 
 #pragma once
 
+#include <queue>
+#include <atomic>
 #include <el/retcode.hpp>
 #include <el/vec.hpp>
 
@@ -22,10 +24,20 @@ protected:
     double current_rotation = 0;
     int configured_speed = 500;
 
+    struct nav_target_t
+    {
+        // distance to drive
+        double distance;
+        // angle to turn
+        double angle;
+    };
+
+    std::queue<nav_target_t> target_queue;
+
 
 public:
-    virtual el::retcode initialize();
-    virtual el::retcode terminate();
+    virtual el::retcode initialize() = 0;
+    virtual el::retcode terminate() = 0;
 
     // === System state getters and setters === //
     virtual const el::vec2_t &getCurrentPosition() const;
@@ -43,10 +55,21 @@ public:
      * @brief rotates the robot by a specific angle.
      * positive is ccw (mathematical angle)
      * 
+     * 
      * @param angle the angle in radians
      */
-    virtual el::retcode rotateBy(double angle);
+    virtual el::retcode rotateBy(double angle) = 0;
 
+    /**
+     * @brief rotates the robot to a specified angle referenced
+     * to the root coordinate system. This will subtract the current angle
+     * from the goal angle and then rotateBy() that delta to reached the
+     * desired goal
+     * 
+     * @param angle absolute angle to reach
+     * @retval ok - rotation started
+     */
+    virtual el::retcode rotateTo(double angle);
     /**
      * @brief drives the robot by a certain distance
      * in it's current direction. Positive is foreward, 
@@ -54,13 +77,25 @@ public:
      * 
      * @param distance distance in cm
      */
-    virtual el::retcode driveDistance(double distance);
+    virtual el::retcode driveDistance(double distance) = 0;
+
+    /**
+     * @brief drives in a straight line by a specific vector relative to the current position 
+     * that is referenced to the root coordinate system. When issued
+     * to drive a certain xy distance, the robot will set it's goal position to the 
+     * current position plus the specified vector. It will then rotate in the direction of
+     * the goal and drive to it in a straight line.
+     * 
+     * @param d delta vector
+     * @retval ok - started driving
+     */
+    virtual el::retcode driveVector(el::vec2_t d);
 
     /**
      * @retval true - last target has been reached (no target active)
      * @retval false - target currently active but it hasen't been reached jet
      */
-    virtual bool targetReached();
+    virtual bool targetReached() = 0;
 
     /**
      * @brief blocks until the currently active target is reached.
@@ -68,7 +103,7 @@ public:
      * 
      * @return el::retcode 
      */
-    virtual el::retcode awaitTargetReached();
+    virtual el::retcode awaitTargetReached() = 0;
 
     /**
      * @brief blocks until the currently active target is completed
@@ -80,6 +115,6 @@ public:
      * @param percent percentage of the goal
      * @return el::retcode 
      */
-    virtual el::retcode awaitTargetPercentage(int percent);
+    virtual el::retcode awaitTargetPercentage(int percent) = 0;
     
 };
